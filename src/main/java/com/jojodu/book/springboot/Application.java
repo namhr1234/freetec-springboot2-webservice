116cef16bffb3343aca05ed7f3a5d7df5b0a3b64
namhr <namhr1234@hanatour.com>
2022-06-15 06:48:11 +0900

    My first "Test Code"

--- src/main/java/com/jojodu/book/springboot/Application.java
+++ src/main/java/com/jojodu/book/springboot/Application.java
@@ -1,7 +1,9 @@
 package com.jojodu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication // 2022.06.14 스프링부트의 자동 설정, 스프링 빈 읽기 생성을 모두 자동으로 해준다!! 프로젝트 최상단에 위치해야 한다!
 public class Application {
 
         public static void main(String[] args){
