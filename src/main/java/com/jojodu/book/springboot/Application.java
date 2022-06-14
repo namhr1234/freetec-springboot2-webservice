6aecaf9d7f0e7318475dc270e28e4b6309f378fd
namhr <namhr1234@hanatour.com>
2022-06-14 23:55:01 +0900

    init

---
+++ src/main/java/com/jojodu/book/springboot/Application.java
@@ -0,0 +1,10 @@
+package com.jojodu.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+
+        public static void main(String[] args){
+            SpringApplication.run(Application.class, args);
+        }
+}
