6f738e25ab4cd75801a2c3b4bd36e1910d981c20
namhr <namhr1234@hanatour.com>
2022-06-16 23:53:40 +0900

    DTO for Lombok test

--- src/main/java/com/jojodu/book/springboot/web/web/dto/HelloResponseDto.java
+++ src/main/java/com/jojodu/book/springboot/web/web/dto/HelloResponseDto.java
@@ -9,4 +9,5 @@ public class HelloResponseDto {
 
     private final String name; //22.06.15 lombok 테스트 해보니 에러났다 . Gradle 5.x이상이였기때문이다. Variable not initialized in the default constructor
     private final int amount;
+
 }
