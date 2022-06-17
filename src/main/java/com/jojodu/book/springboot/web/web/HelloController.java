0b67085cfd4f1a5a985af90eb16bba14d119c9ba
namhr <namhr1234@hanatour.com>
2022-06-17 23:37:52 +0900

    API TEST

--- src/main/java/com/jojodu/book/springboot/web/web/HelloController.java
+++ src/main/java/com/jojodu/book/springboot/web/web/HelloController.java
@@ -1,6 +1,9 @@
 package com.jojodu.book.springboot.web.web;
 
+import com.jojodu.book.springboot.web.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +12,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
     }
 }
